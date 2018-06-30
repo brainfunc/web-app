87ae5374945a2a50318f24516b9f30cfb070f80a
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-06-30 21:07:12 +0530

    Point to prod

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-var ROOT_SERVER_URL = 'http://localhost:3000';
-// var ROOT_SERVER_URL = 'https://brainfunc-landing-page-server.herokuapp.com';
+// var ROOT_SERVER_URL = 'http://localhost:3000';
+var ROOT_SERVER_URL = 'https://brainfunc-web-app-server.herokuapp.com';
 
 export default ROOT_SERVER_URL;
