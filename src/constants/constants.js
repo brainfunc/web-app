4c457a7cb072316f71bd298d1d904b32b56d8bc3
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-07-16 13:38:50 +0530

    added link constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,29 +1,38 @@
 export const Links = {
-  'header': {},
+  'header': {
+    'marketplace':'',
+    'how_to_play':'',
+    'team': 'https://github.com/brainfunc/web-app/blob/dev/docs/team.md',
+    'blog': 'https://medium.com/brainfunc',
+    'jobs': 'https://angel.co/brainfunc/jobs',
+    'faqs':'https://github.com/brainfunc/web-app/blob/dev/docs/faqs.md',
+    'contact':'mailto:tejnikumbh.competitions@gmail.com?subject=BrainFunc%20Contact'
+  },
   'footer': {
     'about': {
-      'whitepaper': 'https://github.com/brainfunc/web-app/blob/dev/docs/whitepaper.md',
       'team': 'https://github.com/brainfunc/web-app/blob/dev/docs/team.md',
-      'blog': '',
-      'jobs': '',
+      'blog': 'https://medium.com/brainfunc',
+      'jobs': 'https://angel.co/brainfunc/jobs',
       'faqs':'https://github.com/brainfunc/web-app/blob/dev/docs/faqs.md',
-      'contact':'',
+      'contact':'mailto:tejnikumbh.competitions@gmail.com?subject=BrainFunc%20Contact'
     },
     'community': {
-      'facebook': '',
-      'twitter': '',
-      'telegram': '',
+      'facebook': 'https://facebook.com/brainfunc',
+      'twitter': 'https://twitter.com/brain_func',
+      'telegram': 'https://t.me/joinchat/G4Ls4hJ5DEMLJTeSH-rMrw',
       'instagram': '',
-      'reddit': '',
-      'youtube': '',
+      'reddit': 'https://reddit.com/r/brainfunc',
+      'youtube': 'https://www.youtube.com/channel/UCKQLoqGwMiepcNxlOQ1-tjg/',
       'slack': '',
-      'linkedin': ''
+      'discord': '',
+      'linkedin': 'https://www.linkedin.com/company/brainfunc/'
     },
     'gameplay': {
-      'concept': '',
-      'metamask': '',
-      'battles': '',
-      'marketplace': ''
+      'overview': 'https://github.com/brainfunc/web-app/blob/dev/docs/gameplay.md',
+      'concepts': 'https://github.com/brainfunc/web-app/blob/dev/docs/gameplay.md',
+      'collectibles': 'https://github.com/brainfunc/web-app/blob/dev/docs/gameplay.md',
+      'marketplace': 'https://github.com/brainfunc/web-app/blob/dev/docs/gameplay.md',
+      'metamask': 'https://github.com/brainfunc/web-app/blob/dev/docs/gameplay.md'
     }
   }
 }
