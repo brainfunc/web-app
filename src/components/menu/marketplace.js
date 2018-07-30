d826c2676a47a1dc4c6cc9c201644c2890f0be95
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-07-30 05:32:44 +0530

    store details complete

--- src/components/menu/marketplace.js
+++ src/components/menu/marketplace.js
@@ -7,13 +7,18 @@ import Footer from '../common/footer';
 import * as Constants from '../../utils/data';
 
 export default class Marketplace extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedPack : ''
+    }
+  }
 
   render() {
     return (
       <div className="marketplace__container">
         <div className='heading'> Neuron Packs</div>
         <div className='description-wrapper'>
-
           <div className='description'>
             The neuron packs give you the ability to purchase a set of neurons.
             These neurons will be a random combination of the 13 types available,
@@ -23,9 +28,7 @@ export default class Marketplace extends Component {
           </div>
         </div>
         <div className='packs-wrapper'>
-
           <div className='pack yellow'>
-
             <div className='title'> Cerebrum </div>
             <img className='pack_image'
               src={`/style/images/${Constants.menuData.marketplace.images.cerebrum_pack}`}/>
@@ -33,9 +36,7 @@ export default class Marketplace extends Component {
               <div className='number'> Neural Price </div>
               <div className='price'> 0.04 Eth</div>
             </div>
-
           </div>
-
           <div className='pack green'>
             <div className='title'> Cerebellum </div>
             <img className='pack_image'
@@ -61,6 +62,32 @@ export default class Marketplace extends Component {
             <div className='pack_description'>
               <div className='number'> Neural Price</div>
               <div className='price'> 0.01 Eth</div>
+            </div>
+          </div>
+        </div>
+        <div className='details-wrapper'>
+          <div className='details-image-wrapper'>
+            <img className='details-image'
+            src={`/style/images/${Constants.menuData.marketplace.images.cerebrum_pack}`}/>
+          </div>
+          <div className='details-description-wrapper'>
+            <div className='title'> {Constants.menuData.marketplace.details.cerebrum.title} </div>
+            <div className='subtitle'> {Constants.menuData.marketplace.details.cerebrum.subtitle} </div>
+            <div className='description'>
+              {Constants.menuData.marketplace.details.cerebrum.description}
+            </div>
+            <div className='price-wrapper'>
+              <div className='quantity'>
+                <button className='qty decrement'> - </button>
+                <div className='qty number'> 1 </div>
+                <button className='qty increment'> + </button>
+              </div>
+              <div className='total-price'>
+                Total Price: {Constants.menuData.marketplace.details.cerebrum.price} Eth
+              </div>
+            </div>
+            <div className='buy-wrapper'>
+              <button className='buy'> Buy Neurons </button>
             </div>
           </div>
         </div>
--- src/utils/data.js
+++ src/utils/data.js
@@ -61,6 +61,46 @@ export const menuData = {
       "cerebellum_pack": "menu/marketplace/cerebellum_pack.png",
       "brainstem_pack":"menu/marketplace/brainstem_pack.png",
       "arterial_pack":"menu/marketplace/arterial_pack.png"
+    },
+    "details" : {
+      "cerebrum": {
+        "title": "Cerebrum",
+        "subtitle": "5 Neuron Types",
+        "description": `This pack contains neurons of either of the 5 types, namely
+        Frontal Lobe(left and right), Occipital Lobe, Temporal Lobe and Parietal Lobe.
+        These 5 parts correspond to the parts of the Cerebrum and will help you compete
+        in battles involving Cerebrum functionality. For details on functionality,
+        checkout the library!`,
+        "price": "0.04"
+      },
+      "cerebellum": {
+        "title": "Cerebellum",
+        "subtitle": "3 Neuron Types",
+        "description": `This pack contains neurons of either of the 3 types, namely
+        Focculonodular lobe, Anterior Lobe and Posterior Lobe. These 3 parts
+        correspond to the parts of the Cerebellum and will help you compete in
+        battles involving Cerebellum related functionality. For details on functionality,
+        checkout the library!`,
+        "price": "0.03"
+      },
+      "brainstem": {
+        "title": "Brainstem",
+        "subtitle": "3 Neuron Types",
+        "description": `This pack contains neurons of either of the 3 types, namely
+        Medulla, Midbrain and Pons. These 3 parts correspond to the parts of the
+        Brainstem and will help you compete in battles involving brainstem
+        related functionality. For details on functionality, checkout the library!`,
+        "price": "0.025"
+      },
+      "arterial": {
+        "title": "Arterial",
+        "subtitle": "2 Neuron Types",
+        "description": `This pack contains neurons of either of the 2 types, namely
+        the Basilar or Vertebral Arteries. They will help you construct these Arteries
+        and aid you in fighting battles that require their functionality. For details on
+        functionality, checkout the library!`,
+        "price": "0.01"
+      }
     }
   }
 }
