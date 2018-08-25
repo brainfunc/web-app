1cdd52e2d30981389034c9fe5e09366a2703bddf
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-08-26 00:59:13 +0530

    Brainpartv1++

--- src/components/menu/sub/my_collectibles_brainpart_stash.js
+++ src/components/menu/sub/my_collectibles_brainpart_stash.js
@@ -4,24 +4,7 @@ import ReactDOM from 'react-dom';
 import * as Collectibles from '../../../utils/data/collectibles';
 import * as Utils from '../../../utils/utils';
 
-import StashBase from "./common/stash_base";
-
-export const BrainpartCard = function(props) {
-  // brainparts/lockedBrainpart3.png
-  var imageSrc = "";
-  if (props.strength == 0) {
-    imageSrc = "/style/images/collectibles/brainparts/lockedBrainpart5.png"
-  } else { imageSrc = props.image }
-
-  return(
-    <div className="card-4">
-      <img className="image" src={imageSrc}/>
-      <div className="title"> {props.subcategory} </div>
-      <div className="description"> Strength Level {props.strength} </div>
-    </div>
-  );
-}
-
+import BrainpartCard from "./my_collectibles_brainpart_stash_brainpart_card";
 
 export const BrainpartCardsRow = function(props) {
   console.log(props);
@@ -29,10 +12,8 @@ export const BrainpartCardsRow = function(props) {
   for(var i = 0; i < props["number"]; i++) {
     cardsArr.push(
       <BrainpartCard key={i}
-      category={props.brainparts[i].category}
-      subcategory={props.brainparts[i].subcategory}
-      strength={props.brainparts[i].strength}
-      image={props.brainparts[i].image}/>
+      part={props.brainparts[i]}
+      selectFunction={props.selectFunction}/>
     );
   }
   return (cardsArr);
@@ -43,19 +24,23 @@ export const BrainpartCardsComponent = function(props) {
     <div className="cards_container-4">
       <div className="card_row-4">
         <BrainpartCardsRow number={4}
-        brainparts={props.brainparts.slice(0,4)}/>
+        brainparts={props.brainparts.slice(0,4)}
+        selectFunction={props.selectFunction}/>
       </div>
       <div className="card_row-4">
         <BrainpartCardsRow number={4}
-        brainparts={props.brainparts.slice(4,8)}/>
+        brainparts={props.brainparts.slice(4,8)}
+        selectFunction={props.selectFunction}/>
       </div>
       <div className="card_row-4">
         <BrainpartCardsRow number={4}
-        brainparts={props.brainparts.slice(8,12)}/>
+        brainparts={props.brainparts.slice(8,12)}
+        selectFunction={props.selectFunction}/>
       </div>
       <div className="card_row-4">
         <BrainpartCardsRow number={2}
-        brainparts={props.brainparts.slice(12,14)}/>
+        brainparts={props.brainparts.slice(12,14)}
+        selectFunction={props.selectFunction}/>
       </div>
     </div>
   );
@@ -66,47 +51,52 @@ export default class BrainpartStash extends Component {
   constructor(props) {
     super(props);
 
-    this.sortBrainPartsByStrength = this.sortBrainPartsByStrength.bind(this);
+    this.state = {
+      selectedBrainpart: Collectibles.Data.Brainparts[0],
+      brainparts: Collectibles.Data.Brainparts
+      .sort(Utils.GetSortOrder("strength"))
+      .reverse()
+    }
+
+    this.SetSelectedBrainpart = this.SetSelectedBrainpart.bind(this);
   }
 
-  getImageForBrainpartDetail() {
-    return "/style/images/collectibles/brainparts/leftfrontal.png";
-  }
-
-  sortBrainPartsByStrength(brainpartsArray) {
-    return brainpartsArray.sort(Utils.GetSortOrder("strength")).reverse();
+  SetSelectedBrainpart(part) {
+    //console.log(part);
+    this.setState({ selectedBrainpart: part })
   }
 
   render() {
-    const brainparts = this.sortBrainPartsByStrength(Collectibles.Data.Brainparts);
     return (
       <div className='brainpart_stash__container'>
         <div className="brainpart_detail_container">
           <div className="image_container">
-            <img src={this.getImageForBrainpartDetail()}/>
+            <img src={this.state.selectedBrainpart.image}/>
+            <button className="boost"
+            onClick = {this.handleBoostClick}> Boost </button>
           </div>
           <div className="description_container">
-            <div className="name"> Frontal Lobe (Left)</div>
+            <div className="name">
+              {this.state.selectedBrainpart.subcategory}
+            </div>
             <div className="details">
               <div className="level">
                 <div className="increase-level-txt">
-                  Strength Level 3
+                  Strength Level {this.state.selectedBrainpart.strength}
                 </div>
                 <div className="locked-status">
                   Locked
                 </div>
               </div>
               <div className="text">
-                The Frontal Lobe performs core cognition functions.It is the most
-                important part in the Cerebrum, that controls important cognitive
-                skills in humans, such as emotional expression, problem solving,
-                memory, language, judgment, and sexual behaviors. The left part
-                is associated with logical cognition.
+                {this.state.selectedBrainpart.description}
               </div>
             </div>
           </div>
         </div>
-        <BrainpartCardsComponent brainparts={brainparts}/>
+        <BrainpartCardsComponent
+        brainparts={this.state.brainparts}
+        selectFunction={this.SetSelectedBrainpart}/>
       </div>
     );
   }
--- src/utils/data/collectibles.js
+++ src/utils/data/collectibles.js
@@ -89,86 +89,144 @@ export const Data = {
     {
       category:"Cerebrum",
       subcategory:"Frontal Lobe(Left)",
-      strength:"7",
+      strength:"5",
       image: "/style/images/collectibles/brainparts/leftfrontal.png",
+      description: `Performs core cognition function - Most important part, that
+      controls important cognitive skills in humans, such as emotional expression,
+      problem solving, memory, language, judgment, and sexual behaviors. The left part
+      is associated with logical cognition.`
     },
     {
       category:"Cerebrum",
       subcategory:"Frontal Lobe(Right)",
-      strength:"7",
+      strength:"5",
       image: "/style/images/collectibles/brainparts/rightfrontal.png",
+      description: `Performs core cognition function - Most important part, that
+      controls important cognitive skills in humans, such as emotional expression,
+      problem solving, memory, language, judgment, and sexual behaviors. The right part
+      is associated with creative abilities.`
     },
     {
       category:"Cerebrum",
       subcategory:"Pareital Lobe",
-      strength:"7",
+      strength:"5",
       image: "/style/images/collectibles/brainparts/pareital.png",
+      description: `Involved in time keeping - The parietal lobes can be divided
+      into two functional regions. One involves sensation and perception and the
+      other is concerned with integrating sensory inputm. The first function
+      integrates sensory information to form a single perception (cognition).
+      This makes it involved in tasks sucj as perception of time, sensation
+      and perception.`
     },
     {
       category:"Cerebrum",
       subcategory:"Temporal Lobe",
-      strength:"7",
+      strength:"5",
       image: "/style/images/collectibles/brainparts/temporal.png",
+      description: `Auditory / Hearing functions - The temporal lobe is involved
+      in primary auditory perception, such as hearing, and holds the primary auditory
+      cortex. The primary auditory cortex receives sensory information from the
+      ears and secondary areas process the information into meaningful units such
+      as speech and words.`
     },
     {
       category:"Cerebrum",
       subcategory:"Occipital Lobe",
-      strength:"7",
+      strength:"5",
       image: "/style/images/collectibles/brainparts/occipital.png",
+      description: `Visual processing function - The occipital lobe is one of the
+      four major lobes of the cerebral cortex in the brain of mammals. The occipital
+      lobe is the visual processing center of the mammalian brain containing most
+      of the anatomical region of the visual cortex.`
     },
     {
       category:"Cerebellum",
       subcategory:"Anterior Lobe",
       strength:"0",
       image: "/style/images/collectibles/brainparts/anteriorlobe.png",
+      description: `Regulates posture - The hemispheres are separated from one
+      another by a thin structure called the vermis, or "worm." The anterior lobe,
+      or paleocerebellum, is the second oldest part of the cerebellum. It receives
+      proprioceptive input from the spinal cord and controls the anti-gravity
+      muscles of the body, thus regulating posture.`
     },
     {
       category:"Cerebellum",
       subcategory:"Posterior Lobe",
       strength:"5",
       image: "/style/images/collectibles/brainparts/posteriorlobe.png",
+      description: `Involved in Kidney and Urinary function - Two hormones are
+      classically considered as being related to the posterior pituitary: oxytocin
+      and vasopressin. These hormones are created in the hypothalamus and released
+      in the posterior pituitary. Bad functioning causes frequent urination and
+      kidney issues.`
     },
     {
       category:"Cerebellum",
       subcategory:"Flocculonodular Lobe",
-      strength:"7",
+      strength:"5",
       image: "/style/images/collectibles/brainparts/flocculonodularlobe.png",
+      description: `Involved in visual processing - The flocculus is a part of
+      the vestibuloocular reflex system and is used to help stabilize gaze during
+      head rotation about any axis of space. Neurons in both the vermis of cerebellum
+      and flocculus transmit an eye velocity signal that correlates with smooth pursuit.`
     },
     {
       category:"Brainstem",
       subcategory:"Midbrain",
       strength:"0",
       image: "/style/images/collectibles/brainparts/midbrain.png",
+      description: `Involved in Auditory Processing - Midbrain, also called mesencephalon,
+      region of the developing vertebrate brain that is composed of the tectum and
+      tegmentum. The midbrain serves important functions in motor movement, particularly
+      movements of the eye, and in auditory processing (also visual processing).`
     },
     {
       category:"Brainstem",
       subcategory:"Pons",
       strength:"0",
       image: "/style/images/collectibles/brainparts/pons.png",
+      description: `Muscle communication, balance co-ordinator - Besides the medulla oblongata,
+      your brainstem also has a structure called the pons. The pons is a major structure
+      in the upper part of your brainstem. It is involved in the control of breathing,
+      communication between different parts of the brain, and sensations such as balance.`
     },
     {
       category:"Brainstem",
       subcategory:"Medulla",
-      strength:"7",
+      strength:"5",
       image: "/style/images/collectibles/brainparts/medulla.png",
+      description: `Info carrier for body - The medulla oblongata helps regulate breathing,
+      heart and blood vessel function, digestion, sneezing, and swalLow Strengthing.
+      This part of the brain is a center for respiration and circulation. Sensory and motor
+      neurons (nerve cells) from the forebrain and midbrain travel through the medulla.`
     },
     {
       category:"Arterial",
       subcategory:"Basilar Artery",
       strength:"0",
       image: "/style/images/collectibles/brainparts/basilarartery.png",
+      description: `Cognition based blood supply. - The basilar artery carries oxygenated
+      blood to the cerebellum, brainstem, and occipital lobes. The cerebellum helps with
+      voluntary processes such as posture, balance, coordination, and speech.`
     },
     {
       category:"Arterial",
       subcategory:"Vertebral Artery",
-      strength:"7",
+      strength:"5",
       image: "/style/images/collectibles/brainparts/vertebralartery.png",
+      description: `Dexterity based blood supply - Each vessel courses superiorly along
+      each side of the neck, merging within the skull to form the single, midline basilar
+      artery. As the supplying component of the vertebrobasilar vascular system, the
+      vertebral arteries provide supply blood to the upper spinal cord, brainstem,
+      cerebellum, and posterior part of brain.`
     },
     {
       category:"Unknown",
       subcategory:"uPart (Soul)",
       strength:"0",
       image: "/style/images/collectibles/brainparts/soulpart.png",
+      description: ``
     }
   ]
 }
