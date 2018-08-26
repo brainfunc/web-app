9b4d5d6f44a0287043595026f9ee2033088cfc4f
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-08-26 11:13:57 +0530

    Exchange Started

--- src/components/menu/sub/my_collectibles_exchange.js
+++ src/components/menu/sub/my_collectibles_exchange.js
@@ -12,7 +12,16 @@ export default class Exchange extends Component {
   render() {
     return (
       <div className='exchange__container'>
-        Exchange
+        <div className='title'> What is this? </div>
+        <div className='description'>
+          {`This is the BrainFunc Exchange. It is meant to be a place where you can
+          use your constructed brainparts (in game) and trade them for collectibles
+          from other games. Exciting isn't it? Now you can use your brainparts not
+          just within BrainFunc but in other games as well. For now, we support the
+          alto cryptochallenge loot (5 items, exchangeable via associated brain parts
+          of strength 1) as shown below. Click on exchange for a transfer to take
+          place. It's as easy as that!`}
+        </div>
       </div>
     );
   }
