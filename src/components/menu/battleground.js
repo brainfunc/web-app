a07376167a53baf9e1de8a19c1ff615936b87816
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-08-28 00:51:31 +0530

    BattleInstructions HTML Complete

--- src/components/menu/battleground.js
+++ src/components/menu/battleground.js
@@ -12,9 +12,52 @@ export default class Battleground extends Component {
     return (
       <div className="battleground__container">
         <div className="header">
-        <img src={`/style/images/menu/battles/battle_icon.png`}/>
-        <div className='title'> Battleground </div>
-        <img src={`/style/images/menu/battles/battle_icon.png`}/>
+          <img src={`/style/images/menu/battles/battle_icon.png`}/>
+          <div className='title'> Battleground </div>
+          <img src={`/style/images/menu/battles/battle_icon.png`}/>
+        </div>
+        <div className='instructions'>
+          <div className='instruction-item'>
+            <div className='instruction-bullet-image-wrapper'>
+              <img className='instruction-bullet-image' src={`/style/images/template/tick.png`}/>
+            </div>
+            <div className='instruction-text'> Text...
+            </div>
+          </div>
+          <div className='instruction-item'>
+            <div className='instruction-bullet-image-wrapper'>
+              <img className='instruction-bullet-image' src={`/style/images/template/tick.png`}/>
+            </div>
+            <div className='instruction-text'> Text...
+            </div>
+          </div>
+          <div className='instruction-item'>
+            <div className='instruction-bullet-image-wrapper'>
+              <img className='instruction-bullet-image' src={`/style/images/template/tick.png`}/>
+            </div>
+            <div className='instruction-text'> Text...
+            </div>
+          </div>
+          <div className='instruction-item'>
+            <div className='instruction-bullet-image-wrapper'>
+              <img className='instruction-bullet-image' src={`/style/images/template/tick.png`}/>
+            </div>
+            <div className='instruction-text'> Text...
+            </div>
+          </div>
+        </div>
+        <div className='start-footer'>
+          <div className='footer-image-container left'>
+            <img className='footer-image' src={`/style/images/icons/submission_footer.png`}/>
+          </div>
+          <div className='start-button-wrapper'>
+            <button className='start-button'>
+              Start
+            </button>
+          </div>
+          <div className='footer-image-container right'>
+            <img className='footer-image' src={`/style/images/icons/submission_footer.png`}/>
+          </div>
         </div>
       </div>
     );
